f0e600123edc43a1a031acd01a623e3e97638385
Maria Komarova <komarova@phystech.edu>
2021-11-10 17:12:25 +0200

    Rewrite through UserDataHandler class

--- bot.py
+++ bot.py
@@ -1,10 +1,7 @@
 import telebot
 from telebot import types
-from telebot.apihelper import ApiTelegramException
-import json
-from collections import OrderedDict
-from searcher import get_matched
 from decouple import config
+from handler import UserDataHandler
 
 
 bot = telebot.TeleBot(config("TOKEN"))
@@ -36,7 +33,7 @@ def handle_command(message):
             'Чтобы узнать как пользоваться, жми "Инструкция".',
             reply_markup=keyboard,
         )
-        _write_channels(user_id, {})
+        UserDataHandler(user_id).write_channels({})
 
 
 @bot.message_handler(
@@ -52,7 +49,9 @@ def handle_command(message):
 )
 def get_message(message):
     user_id = message.from_user.id
-    last_message = _read_last_message(user_id)
+    user_data_handler = UserDataHandler(user_id)
+    last_message = user_data_handler.read_last_message()
+
     if message.text == "Инструкция":
         bot.send_message(
             user_id,
@@ -70,8 +69,11 @@ def get_message(message):
             text="Сортировать по алфавиту", callback_data="sort"
         )
         inline_keyboard.add(button_sort)
+
         bot.send_message(
-            user_id, display_channels(user_id), reply_markup=inline_keyboard
+            user_id,
+            user_data_handler.display_channels(user_id),
+            reply_markup=inline_keyboard,
         )
     elif message.text == "Поиск по названию":
         bot.send_message(user_id, "Напиши ключевое слово")
@@ -86,18 +88,22 @@ def get_message(message):
         bot.send_message(
             user_id,
             "Каналы нельзя будет вернуть. Чтобы подтвердить удаление всех-всех"
-            '                 каналов, нажми "Удалить все каналы" ещё раз',
+            ' каналов, нажми "Удалить все каналы" ещё раз',
         )
     elif (
         message.text == "Удалить все каналы"
         and last_message == "Удалить все каналы"
     ):
-        delete_all_channels(user_id)
+        user_data_handler.delete_all_channels()
         bot.send_message(user_id, "Список каналов пуст.")
     elif last_message == "Поиск по названию":
-        bot.send_message(user_id, search_channels(user_id, message.text))
+        bot.send_message(
+            user_id, user_data_handler.search_channels(message.text)
+        )
     elif last_message == "Добавить канал вручную":
-        bot.send_message(user_id, add_channel_by_link(user_id, message.text))
+        bot.send_message(
+            user_id, user_data_handler.add_channel_by_link(message.text, bot)
+        )
     elif last_message == "Удалить канал":
         inline_keyboard = types.InlineKeyboardMarkup()
         button_yes = types.InlineKeyboardButton(
@@ -105,10 +111,9 @@ def get_message(message):
         )
         button_no = types.InlineKeyboardButton(text="Нет", callback_data="no")
         inline_keyboard.add(button_yes, button_no)
-        answer = search_channels(user_id, message.text, max=1)
-        bot.send_message(
-            user_id, search_channels(user_id, message.text, max=1)
-        )
+
+        answer = user_data_handler.search_channels(message.text, max_num=1)
+        bot.send_message(user_id, answer)
         if answer != "Таких каналов не найдено, попробуй другое слово.":
             bot.send_message(
                 user_id, "Удалить этот канал?", reply_markup=inline_keyboard
@@ -122,157 +127,34 @@ def get_message(message):
     else:
         # technical message
         # bot.send_message(user_id, message.forward_from_chat)
-        answer = add_channel(user_id, message.forward_from_chat)
+        answer = user_data_handler.add_channel(message.forward_from_chat)
         bot.send_message(user_id, answer, reply_markup=keyboard)
-    _write_last_message(user_id, message.text)
+
+    if message.text is None or len(message.text) > 30:
+        new_last_message = ""
+    else:
+        new_last_message = message.text
+
+    user_data_handler.write_last_message(new_last_message)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     user_id = call.message.chat.id
+    user_data_handler = UserDataHandler(user_id)
     if call.data == "sort":
-        answer = display_channels(user_id, to_sort=True)
+        answer = user_data_handler.display_channels(to_sort=True)
         bot.send_message(user_id, answer)
     elif call.data == "no":
         bot.send_message(user_id, "Нет так нет.")
     elif isinstance(call.data, str):
-        delete_matched_channel(user_id, call.data)
+        user_data_handler.delete_matched_channel(call.data)
         bot.send_message(user_id, "Удалил!")
-
-
-def add_channel(user_id, chat):
-    channels = _read_channels(user_id)
-    if chat.username is None:
-        return "Не могу достать ссылку, вероятно канал засекречен."
-    else:
-        channels[chat.title] = f"t.me/{chat.username}"
-        _write_channels(user_id, channels)
-        return f"Добавлен канал {chat.title}!"
-
-
-def add_channel_by_link(user_id, message):
-    channels = _read_channels(user_id)
-    link = message.split(" ")[-1]
-    if "t.me/" not in link:
-        return (
-            'Не вижу ссылки. Должна быть либо ссылка, либо "Название ссылка",'
-            " через пробел."
-        )
-    if link != message:
-        title = " ".join(message.split(" ")[:-1])
-    else:
-        channel_id = "@" + link.split("/")[-1]
-        try:
-            channel_info = bot.get_chat(channel_id)
-        except ApiTelegramException:
-            return (
-                "Не могу достать название, вероятно канал засекречен. Нажми"
-                ' "Добавить канал вручную" и пришли "Название ссылка", через'
-                " пробел."
-            )
-        title = channel_info.title
-    channels[title] = link
-    _write_channels(user_id, channels)
-    return f"Добавлен канал {title}!"
-
-
-def add_channel_with_title(user_id, title, link):
-    channels = _read_channels(user_id)
-    channels[title] = link
-    _write_channels(user_id, channels)
-    return f"Добавлен канал {title}!"
-
-
-def display_channels(user_id, to_sort=False):
-    channels = _read_channels(user_id)
-    if to_sort:
-        channels = OrderedDict(sorted(channels.items()))
-    return _create_message_from_channels(channels)
-
-
-def search_channels(user_id, text, max=None):
-    channels = _read_channels(user_id)
-    chan_names = get_matched(text, channels.keys())
-    if max is not None:
-        chan_names = chan_names[:1]
-    found_channels = {name: channels[name] for name in chan_names}
-    return _create_message_from_channels(
-        found_channels,
-        no_channels_message="Таких каналов не найдено, попробуй другое слово.",
-    )
-
-
-def delete_matched_channel(user_id, text):
-    channels = _read_channels(user_id)
-    chan_name = get_matched(text, channels.keys())[0]
-    del channels[chan_name]
-    _write_channels(user_id, channels)
-
-
-def delete_all_channels(user_id):
-    _write_channels(user_id, {})
-
-
-def _create_message_from_channels(channels, no_channels_message=None):
-    if no_channels_message is None:
-        no_channels_message = (
-            "Каналы не добавлены! Чтобы добавить канал, перешли мне сообщение"
-            " из него."
-        )
-    if len(channels) == 0:
-        return no_channels_message
-    channels_list = [
-        f"{title}:\n\t\t\t\t\t\t\t\t\t{link}"
-        for title, link in channels.items()
-    ]
-    return "\n".join(channels_list)
-
-
-def _read_last_message(user_id):
-    with open("channels.json") as f:
-        users_channels = json.load(f)
-    if len(users_channels) != 0:
-        return users_channels[str(user_id)]["last_message"]
-    else:
-        return None
-
-
-def _read_channels(user_id):
-    with open("channels.json") as f:
-        users_channels = json.load(f)
-    if len(users_channels) != 0:
-        return users_channels[str(user_id)]["channels"]
-    else:
-        return {}
-
-
-def _write_channels(user_id, channels):
-    with open("channels.json", "r+") as f:
-        users_channels = json.load(f)
-        if str(user_id) not in users_channels:
-            users_channels[str(user_id)] = {
-                "channels": {},
-                "last_message": "/start",
-            }
-
-        users_channels[str(user_id)]["channels"] = channels
-        f.seek(0)
-        json.dump(users_channels, f)
-        f.truncate()
-
-
-def _write_last_message(user_id, last_message):
-    with open("channels.json", "r+") as f:
-        users_channels = json.load(f)
-        users_channels[str(user_id)]["last_message"] = last_message
-        f.seek(0)
-        json.dump(users_channels, f)
-        f.truncate()
 
 
 bot.polling(none_stop=True, interval=0)
 
-# TODO: обернуть операции с каналами в класс
+# TODO: порефакторить
 # TODO: подключить бд
 # TODO: залить на сервер
 # TODO: добавить логирование
---
+++ handler.py
@@ -0,0 +1,150 @@
+import json
+from collections import OrderedDict
+from telebot.apihelper import ApiTelegramException
+from searcher import get_matched
+
+
+class UserDataHandler(object):
+    def __init__(self, user_id) -> None:
+        self.user_id = str(user_id)
+
+    def add_channel(self, channel: dict) -> str:
+        channels = self._read_channels()
+
+        if channel.username is None:
+            return "Не могу достать ссылку, вероятно канал засекречен."
+
+        channels[channel.title] = f"t.me/{channel.username}"
+        self.write_channels(channels)
+        return f"Добавлен канал {channel.title}!"
+
+    def add_channel_by_link(self, message, bot) -> str:
+        channels = self._read_channels()
+        link = message.split(" ")[-1]
+
+        if "t.me/" not in link:
+            return (
+                'Не вижу ссылки. Должна быть либо ссылка, либо "Название'
+                ' ссылка", через пробел.'
+            )
+
+        if link != message:
+            title = " ".join(message.split(" ")[:-1])
+        else:
+            channel_id = "@" + link.split("/")[-1]
+            try:
+                channel_info = bot.get_chat(channel_id)
+            except ApiTelegramException:
+                return (
+                    "Не могу достать название, вероятно канал засекречен."
+                    ' Нажми "Добавить канал вручную" и пришли "Название'
+                    ' ссылка", через пробел.'
+                )
+            title = channel_info.title
+
+        channels[title] = link
+        self.write_channels(channels)
+        return f"Добавлен канал {title}!"
+
+    def add_channel_with_title(self, title, link) -> str:
+        channels = self._read_channels()
+        channels[title] = link
+        self.write_channels(channels)
+        return f"Добавлен канал {title}!"
+
+    def display_channels(self, to_sort=False) -> str:
+        channels = self._read_channels()
+        if to_sort:
+            channels = OrderedDict(sorted(channels.items()))
+        return self._create_message_from_channels(channels)
+
+    def search_channels(self, keyword, max_num=None) -> str:
+        channels = self._read_channels()
+        chan_names = get_matched(keyword, channels.keys())
+
+        if max_num is not None:
+            chan_names = chan_names[:max_num]
+
+        found_channels = {name: channels[name] for name in chan_names}
+
+        return self._create_message_from_channels(
+            found_channels,
+            no_channels_message=(
+                "Таких каналов не найдено, попробуй другое слово."
+            ),
+        )
+
+    def delete_matched_channel(self, text) -> None:
+        channels = self._read_channels()
+        chan_name = get_matched(text, channels.keys())[0]
+        del channels[chan_name]
+        self.write_channels(channels)
+
+    def delete_all_channels(self) -> None:
+        self.write_channels({})
+
+    def _create_message_from_channels(
+        self, channels, no_channels_message=None
+    ) -> str:
+        if no_channels_message is None:
+            no_channels_message = (
+                "Каналы не добавлены! Чтобы добавить канал, перешли мне"
+                " сообщение из него."
+            )
+
+        if len(channels) == 0:
+            return no_channels_message
+
+        channels_list = [
+            f"{title}:\n\t\t\t\t\t\t\t\t\t{link}"
+            for title, link in channels.items()
+        ]
+        return "\n".join(channels_list)
+
+    def _read_channels(self) -> dict:
+        with open("channels.json") as f:
+            users_channels = json.load(f)
+
+        if self.user_id not in users_channels:
+            self._add_new_user(users_channels)
+
+        return users_channels[self.user_id]["channels"]
+
+    def read_last_message(self) -> str:
+        with open("channels.json") as f:
+            users_channels = json.load(f)
+
+        if self.user_id not in users_channels:
+            self._add_new_user(users_channels)
+
+        return users_channels[self.user_id]["last_message"]
+
+    def write_channels(self, channels) -> None:
+        with open("channels.json", "r+") as f:
+            users_channels = json.load(f)
+
+            if self.user_id not in users_channels:
+                self._add_new_user(users_channels)
+
+            users_channels[self.user_id]["channels"] = channels
+            f.seek(0)
+            json.dump(users_channels, f)
+            f.truncate()
+
+    def write_last_message(self, last_message) -> None:
+        with open("channels.json", "r+") as f:
+            users_channels = json.load(f)
+
+            if self.user_id not in users_channels:
+                self._add_new_user(users_channels)
+
+            users_channels[self.user_id]["last_message"] = last_message
+            f.seek(0)
+            json.dump(users_channels, f)
+            f.truncate()
+
+    def _add_new_user(self, users_channels) -> None:
+        users_channels[self.user_id] = {
+            "channels": {},
+            "last_message": "/start",
+        }
